5a8d778fb241aee9d4967fec81b468ce3d6fdc47
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-15 14:38:36 -0500

    Add models

---
+++ api/src/models/TouristActivity.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize")
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("touristActivity", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    difficulty: {
+      type: DataTypes.ENUM({
+        values: ["1", "2", "3", "4", "5"],
+      }),
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    season: {
+      type: DataTypes.ENUM({
+        values: ["Verano", "Otoño", "Invierno", "Primavera"],
+      }),
+      allowNull: false,
+    },
+  })
+}
