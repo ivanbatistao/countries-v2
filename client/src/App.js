98470f0d27c54a50557e563c61121ea9732d4336
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-17 10:26:09 -0500

    Set initial App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import React from "react"
+import "./App.css"
+
+import InitialPage from "./components/InitialPage/InitialPage"
+import Home from "./components/Home/Home"
+import Countries from "./components/Countries/Countries"
+import CountriesSearch from "./components/CountriesSearch/CountriesSearch"
+import CountryDetails from "./components/CountryDetails/CountryDetails"
+
+import { Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
+      <Route exact path="/" component={InitialPage} />
+      <Route path="/countries">
+        <header className="App-header">
+          <Home />
+        </header>
+        <Countries />
+        <CountriesSearch/>
+      </Route>
+      <Route path="/country/:id" component={CountryDetails} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
