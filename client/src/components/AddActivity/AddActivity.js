a7756c4b3b5cd7733fe312e00cd754c7ee62e10c
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-18 18:07:39 -0500

    Create component AddActivity

---
+++ client/src/components/AddActivity/AddActivity.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react"
+
+function AddActivity() {
+  const [input, setInput] = useState({
+    name: "",
+    difficulty: "",
+    duration: "",
+    season: "",
+    countries: "",
+  })
+
+  function handleInputChange(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    try {
+      let { name, difficulty, duration, season, countries } = input
+      let body = { name, difficulty, duration, season, countries }
+
+      const response = await fetch("http://localhost:3001/activity", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      })
+    } catch (error) {
+      console.error(error.message)
+    }
+    setInput({
+      name: "",
+      difficulty: "",
+      duration: "",
+      season: "",
+      countries: "",
+    })
+  }
+
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <div>
+        <label>Activity:</label>
+        <input
+          type="text"
+          name="name"
+          onChange={handleInputChange}
+          value={input.name}
+        />
+      </div>
+      <div>
+        <label>Difficulty:</label>
+        <input
+          type="number"
+          step="0"
+          min="1"
+          max="5"
+          name="difficulty"
+          onChange={handleInputChange}
+          value={input.difficulty}
+        />
+      </div>
+      <div>
+        <label>Duration (hours):</label>
+        <input
+          type="number"
+          step="0.01"
+          min="0"
+          name="duration"
+          onChange={handleInputChange}
+          value={input.duration}
+        />
+      </div>
+      <div>
+        <label Forhtml="season">Season</label>
+        <select name="season" id="season" onChange={handleInputChange}>
+          {/* onChange={(e) => handleOption(e)} */}
+          <option value="">Choose a season</option>
+          <option value="Summer">Summer</option>
+          <option value="Fall">Fall</option>
+          <option value="Winter">Winter</option>
+          <option value="Spring">Spring</option>
+        </select>
+      </div>
+      <div>
+        <label>Countries codes:</label>
+        <input
+          type="text"
+          name="countries"
+          placeholder="Ex: COL, MEX"
+          onChange={handleInputChange}
+          value={input.countries}
+        />
+      </div>
+      <div>
+        <input type="submit" value="Submit" />
+      </div>
+    </form>
+  )
+}
+
+export default AddActivity
