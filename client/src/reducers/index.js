9b49028188a728e091dafa3564ceb2c2cb076eb8
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-18 18:05:12 -0500

    Modify reducer to manage filter by continent

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,38 +3,77 @@ import {
   GET_ALL_COUNTRIES,
   GET_COUNTRY_DETAILS,
   GET_COUNTRIES_SEARCH,
+  GET_PAGES,
   FILTER_BY_CONTINENT,
   FILTER_BY_TOURIST_ACTIVITY,
-  FILTER_ASC_ALPHABET,
-  FILTER_DESC_ALPHABET,
-  FILTER_ASC_POPULATION,
-  FILTER_DESC_POPULATION,
+  ORDER_ASC_ALPHABET,
+  ORDER_DESC_ALPHABET,
+  ORDER_ASC_POPULATION,
+  ORDER_DESC_POPULATION,
   ADD_TOURIST_ACTIVITY,
 } from "../actions/action-types"
+
+const numPages = 24
 
 const initialState = {
   countries: [],
   countriesSearch: [],
-  countryDetails: {}
+  countryDetails: {},
+  pages: " "
+    .repeat(numPages)
+    .split(" ")
+    .map((item, i) => i),
+  byContinent: [],
+  byTouristActivity: []
 }
 
 export default function rootReducer(state = initialState, action) {
+  console.log("action reducer outside case:", action)
   switch (action.type) {
     case GET_TEN_COUNTRIES:
       return {
         ...state,
         countries: state.countries.concat(action.payload),
+        byContinent: state.countries.concat(action.payload),
       }
     case GET_COUNTRIES_SEARCH:
       return {
         ...state,
         countriesSearch: action.payload,
       }
-      case "GET_COUNTRY_DETAILS":
+    case GET_COUNTRY_DETAILS:
+      return {
+        ...state,
+        countryDetails: action.payload,
+      }
+    case GET_PAGES:
+      return {
+        ...state,
+        countries: action.payload,
+        byContinent: action.payload,
+      }
+    case FILTER_BY_CONTINENT:
+      if (action.payload === "all") {
         return {
           ...state,
-          countryDetails: action.payload,
+          byContinent: state.countries,
         }
+      } else {
+        return {
+          ...state,
+          byContinent: state.countries.filter(
+            (country) => country.continent === action.payload
+          ),
+        }
+      }
+    // case FILTER_BY_TOURIST_ACTIVITY:
+    //   console.log("action reducer inside case:", action)
+    //   return {
+    //     ...state,
+    //     byTouristActivity: state.byContinent.filter(
+    //       (country) => country.touristActivities.map(touristActivity => touristActivity.name === action.payload) 
+    //     )
+    //   }
     default:
       return state
   }
