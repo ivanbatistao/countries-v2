a5cf3f51a2ba94cd0c107164c645439e2e0b8711
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-18 18:22:40 -0500

    Add html tag with link to head to /countries/addactivity

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Search from "../Search/Search";
+import { Link } from "react-router-dom"
 
 export default function NavBar() {
   return (
     <div className="nav-bar" >
         <Search />
+        <Link to={`/countries/addactivity`}>
+          <div>Add Tourist Activity</div>
+        </Link>
     </div>
   )
 }
