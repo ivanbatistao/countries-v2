8294040e7aa96a7aa01578888f148690d1622699
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-18 17:53:20 -0500

    Add new components to render

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,29 @@ import Home from "./components/Home/Home"
 import Countries from "./components/Countries/Countries"
 import CountriesSearch from "./components/CountriesSearch/CountriesSearch"
 import CountryDetails from "./components/CountryDetails/CountryDetails"
+import FilterByContinent from "./components/FilterByContinent/FilterByContinent"
+
+import Pages from "./components/Pages/Pages"
 
 import { Route } from "react-router-dom"
+import FilterByTouristActivity from "./components/FilterByTouristActivity/FilterByTouristActivity"
+import AddActivity from "./components/AddActivity/AddActivity"
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={InitialPage} />
-      <Route path="/countries">
+      <Route exact path="/countries">
         <header className="App-header">
           <Home />
         </header>
+        <Pages />
+        <FilterByContinent />
+        <FilterByTouristActivity />
         <Countries />
-        <CountriesSearch/>
+        <CountriesSearch />
       </Route>
+      <Route path="/countries/addactivity" component={AddActivity} />
       <Route path="/country/:id" component={CountryDetails} />
     </div>
   )
