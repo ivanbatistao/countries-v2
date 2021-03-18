fd98d5e03609489911e223756b744a9131553cfd
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-18 17:59:50 -0500

    Add action to manage filter by continent

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,18 +2,19 @@ import {
   GET_TEN_COUNTRIES,
   GET_COUNTRIES_SEARCH,
   GET_COUNTRY_DETAILS,
+  GET_PAGES,
   FILTER_BY_CONTINENT,
   FILTER_BY_TOURIST_ACTIVITY,
-  FILTER_ASC_ALPHABET,
-  FILTER_DESC_ALPHABET,
-  FILTER_ASC_POPULATION,
-  FILTER_DESC_POPULATION,
+  ORDER_ASC_ALPHABET,
+  ORDER_DESC_ALPHABET,
+  ORDER_ASC_POPULATION,
+  ORDER_DESC_POPULATION,
   ADD_TOURIST_ACTIVITY,
 } from "../actions/action-types"
 
 export function getTenCountries() {
   return function (dispatch) {
-    return fetch("http://localhost:3001/countries")
+    return fetch(`http://localhost:3001/countries`)
       .then((response) => response.json())
       .then((json) => {
         dispatch({ type: GET_TEN_COUNTRIES, payload: json })
@@ -36,7 +37,7 @@ export function getCountriesSearch(name) {
     return fetch(`http://localhost:3001/countries?name=${name}`)
       .then((response) => response.json())
       .then((json) => {
-        dispatch({ type: "GET_COUNTRIES_SEARCH", payload: json })
+        dispatch({ type: GET_COUNTRIES_SEARCH, payload: json })
       })
   }
 }
@@ -51,12 +52,52 @@ export function getCountryDetails(id) {
   }
 }
 
-// export function getCountryPagination(offset) {
+// export function getCountriesPage(page) {
 //   return function (dispatch) {
-//     return fetch(`http://localhost:3001/countries?offset=${offset}`)
+//     return fetch(`http://localhost:3001/countries?page=${page}`)
 //       .then((response) => response.json())
 //       .then((json) => {
-//         dispatch({ type: GET_COUNTRY_DETAILS, payload: json })
+//         dispatch({ type: GET_PAGES, payload: json })
 //       })
 //   }
 // }
+
+export function getCountryPagination(page) {
+  return function (dispatch) {
+    return fetch(`http://localhost:3001/countries?page=${page}`)
+      .then((response) => response.json())
+      .then((json) => {
+        dispatch({ type: GET_PAGES, payload: json })
+      })
+  }
+}
+
+export function filterByContinent(payload) {
+  return {
+    type: FILTER_BY_CONTINENT,
+    payload: payload,
+  }
+}
+
+export function filterByTouristActivity(payload) {
+  return {
+    type: FILTER_BY_TOURIST_ACTIVITY,
+    payload: payload,
+  }
+}
+
+// export async function postTouristActivity(payload) {
+//   try {
+//     let { name, difficulty, duration, season, countries } = payload
+//     let body = { name, difficulty, duration, season, countries }
+//     const response = await fetch("http://localholst:3001/activity", {
+//       method: "POST",
+//       header: { "Content-Type": "application/json" },
+//       body: JSON.stringify(body),
+//     })
+//     // window.location = "/"
+//   } catch (error) {
+//     console.error(error.message)
+//   }
+// }
+
