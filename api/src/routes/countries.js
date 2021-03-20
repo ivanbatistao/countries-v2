2f369acffd5f3e845130d798cf19679fdc7b555e
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-20 11:51:06 -0500

    Add a new route to manage tourist activities

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -7,7 +7,8 @@ const app = Router()
 
 app.get("/countries", async (req, res) => {
   try {
-    const { name, page } = req.query
+    const { name, page, activity } = req.query
+    console.log(activity)
 
     let countries = await Country.findAll()
     if (countries.length === 0) {
@@ -26,17 +27,42 @@ app.get("/countries", async (req, res) => {
       })
     }
 
-    if(page) {
+    if (activity) {
+      const allCountriesActivity = await TouristActivity.findAll({
+        include: { model: Country },
+        attributes: {
+          exclude: [
+            "createdAt",
+            "updatedAt",
+            "id",
+            "name",
+            "difficulty",
+            "duration",
+            "season",
+          ],
+        },
+        where: {
+          name: { [Op.iLike]: `${activity}` },
+        },
+      })
+      if (allCountriesActivity.length > 0) {
+        res.json(allCountriesActivity)
+      } else {
+        res.json({ message: "Activity Not Found" })
+      }
+    }
+
+    if (page) {
       const pageSize = 10
       const allCountriesPage = await Country.findAll({
         include: { model: TouristActivity },
         attributes: { exclude: ["createdAt", "updatedAt"] },
         limit: pageSize,
-        offset: Number(page) * pageSize
+        offset: page * pageSize,
       })
       if (allCountriesPage.length > 0) {
         res.json(allCountriesPage)
-      } 
+      }
     }
 
     if (!name) {
@@ -44,11 +70,10 @@ app.get("/countries", async (req, res) => {
         include: { model: TouristActivity },
         attributes: { exclude: ["createdAt", "updatedAt"] },
         limit: 10,
-        offset: 0
+        offset: 0,
       })
       res.json(tenCountries)
-    } 
-    else {
+    } else {
       const matchingCountries = await Country.findAll({
         include: { model: TouristActivity },
         attributes: { exclude: ["createdAt", "updatedAt"] },
@@ -85,34 +110,5 @@ app.get("/countries/:id", async (req, res) => {
     console.error(error.message)
   }
 })
-
-// app.get("/allcountries", async (req, res) => {
-//   try {
-//     let countries = await Country.findAll()
-//     if (countries.length === 0) {
-//       countries = await axios.get("https://restcountries.eu/rest/v2/all")
-//       countries = await countries.data.map(async (obj) => {
-//         await Country.create({
-//           id: obj.alpha3Code,
-//           name: obj.name,
-//           flag: obj.flag,
-//           continent: obj.region,
-//           capital: obj.capital,
-//           subregion: obj.subregion,
-//           area: parseInt(obj.area) ? parseInt(obj.area) : 0,
-//           population: obj.population,
-//         })
-//       })
-//     }
-
-//     const allCountries = await Country.findAll({
-//       include: { model: TouristActivity },
-//       attributes: { exclude: ["createdAt", "updatedAt"] },
-//     })
-//     res.json(allCountries)
-//   } catch (error) {
-//     console.error(error.message)
-//   }
-// })
 
 module.exports = app
