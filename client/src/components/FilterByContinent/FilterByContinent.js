2bc73d73978376c26cdd7d8b529f83c6a744deaf
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-22 05:29:21 -0500

    Modify component FilterByContinent

--- client/src/components/FilterByContinent/FilterByContinent.js
+++ client/src/components/FilterByContinent/FilterByContinent.js
@@ -1,27 +1,39 @@
-import React, { useState } from "react"
-import { useDispatch } from "react-redux"
-import { filterByContinent } from "../../actions/index"
+import React, { Fragment } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+import { filterByContinent,  getCountryPagination } from "../../actions/index"
 
 function FilterByContinent() {
   const dispatch = useDispatch()
-
+  let { stateChooseContinent, pages } = useSelector((state) => state)
+  
   function handleOption(e) {
     e.preventDefault()
     dispatch(filterByContinent(e.target.value))
   }
 
-  return (
-    <label htmlFor="continents"> Choose a continent
-      <select id="continents" onChange={(e) => handleOption(e)}>
-        <option value="all">All</option>
-        <option value="Africa">Africa</option>
-        <option value="Americas">Americas</option>
-        <option value="Asia">Asia</option>
-        <option value="Europe">Europe</option>
-        <option value="Oceania">Oceania</option>
-      </select>
-    </label>
-  )
+  if (stateChooseContinent === "inactive") {
+    return (
+      <Link to="/countries">
+        <button onClick="window.location.reload()">HOME</button>
+      </Link>
+    )
+  } else if (stateChooseContinent === "active" || stateChooseContinent === "") {
+    return (
+      <label htmlFor="continents">
+        {" "}
+        Choose a continent
+        <select id="continents" onChange={(e) => handleOption(e)}>
+          <option value="all">All</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </label>
+    )
+  }
 }
 
 export default FilterByContinent
