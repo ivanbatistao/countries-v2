8fedf3408d10c9acb19182cff674e775bd7a80a7
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-22 05:31:11 -0500

    Modify component Search

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,12 +1,11 @@
-import React, { Fragment, useEffect, useState } from "react"
-import { connect, useDispatch, useSelector } from "react-redux"
+import React, { Fragment, useState } from "react"
+import { useDispatch } from "react-redux"
 // import style from './Cards.module.css'
 // import Card from "../Card/Card"
 import { getCountriesSearch } from "../../actions/index"
 
 export default function Search() {
   const dispatch = useDispatch()
-  // const { countriesSearch } = useSelector((state) => state)
 
   const [name, setName] = useState("")
 
@@ -16,22 +15,26 @@ export default function Search() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    dispatch(getCountriesSearch(name))
-    setName("")
+    if (name === "") {
+      alert("Type a name")
+    } else {
+      dispatch(getCountriesSearch(name))
+      setName("")
+    }
   }
 
-    return (
-      <Fragment>
-        <form className="search-bottom" onSubmit={(e) => handleSubmit(e)}>
-          <input
-            className="search-input"
-            placeholder=" Type here..."
-            type="text"
-            value={name}
-            onChange={(e) => handleChange(e)}
-          />
-          <input className="search-submit" type="submit" value="SEARCH" />
-        </form>
-      </Fragment>
-    )
+  return (
+    <Fragment>
+      <form className="search-bottom" onSubmit={(e) => handleSubmit(e)}>
+        <input
+          className="search-input"
+          placeholder=" Type here..."
+          type="text"
+          value={name}
+          onChange={(e) => handleChange(e)}
+        />
+        <input className="search-submit" type="submit" value="SEARCH" />
+      </form>
+    </Fragment>
+  )
 }
