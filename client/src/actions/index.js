62c74e44f6266e8427132b94a3724ead2e3fe65d
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-22 05:26:51 -0500

    Add new actions and modify existing actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -10,69 +10,54 @@ import {
 } from "../actions/action-types"
 
 export function getTenCountries() {
-  return function (dispatch) {
-    return fetch(`http://localhost:3001/countries`)
-      .then((response) => response.json())
-      .then((json) => {
-        dispatch({ type: GET_TEN_COUNTRIES, payload: json })
-      })
+  return async function (dispatch) {
+    const response = await fetch(`http://localhost:3001/countries`)
+    const json = await response.json()
+    dispatch({ type: GET_TEN_COUNTRIES, payload: json })
   }
 }
 
-// export function getCountries(name) {
-//   return function (dispatch) {
-//     return fetch(`http://localhost:3001/countries/${name}`)
-//       .then((response) => response.json())
-//       .then((json) => {
-//         dispatch({ type: GET_COUNTRIES_SEARCH, payload: json })
-//       })
-//   }
-// }
-
 export function getCountriesSearch(name) {
-  return function (dispatch) {
-    return fetch(`http://localhost:3001/countries?name=${name}`)
-      .then((response) => response.json())
-      .then((json) => {
-        dispatch({ type: GET_COUNTRIES_SEARCH, payload: json })
-      })
+  return async function (dispatch) {
+    const response = await fetch(`http://localhost:3001/countries?name=${name}`)
+    const json = await response.json()
+    dispatch({ type: GET_COUNTRIES_SEARCH, payload: json })
   }
 }
 
 export function getCountryDetails(id) {
-  return function (dispatch) {
-    return fetch(`http://localhost:3001/countries/${id}`)
-      .then((response) => response.json())
-      .then((json) => {
-        dispatch({ type: GET_COUNTRY_DETAILS, payload: json })
-      })
+  return async function (dispatch) {
+    const response = await fetch(`http://localhost:3001/countries/${id}`)
+    const json = await response.json()
+    dispatch({ type: GET_COUNTRY_DETAILS, payload: json })
   }
 }
 
-export function getCountryPagination(page) {
-  return function (dispatch) {
-    return fetch(`http://localhost:3001/countries?page=${page}`)
-      .then((response) => response.json())
-      .then((json) => {
-        dispatch({ type: GET_PAGES, payload: json })
-      })
+export function getCountryPagination(page, continent) {
+  return async function (dispatch) {
+    const response = await fetch(
+      `http://localhost:3001/countries?page=${page}&&continent=${continent}`
+    )
+    const json = await response.json()
+    dispatch({ type: GET_PAGES, payload: json })
   }
 }
 
-export function filterByContinent(payload) {
-  return {
-    type: FILTER_BY_CONTINENT,
-    payload: payload,
+export function filterByContinent(continent) {
+  return async function (dispatch) {
+    const response = await fetch(`http://localhost:3001/allCountries`)
+    const json = await response.json()
+    dispatch({ type: FILTER_BY_CONTINENT, payload: { json, continent } })
   }
 }
 
 export function filterByTouristActivity(touristActivity) {
-  return function (dispatch) {
-    return fetch(`http://localhost:3001/countries?activity=${touristActivity}`)
-      .then((response) => response.json())
-      .then((json) => {
-        dispatch({ type: FILTER_BY_TOURIST_ACTIVITY, payload: json })
-      })
+  return async function (dispatch) {
+    const response = await fetch(
+      `http://localhost:3001/countries?activity=${touristActivity}`
+    )
+    const json = await response.json()
+    dispatch({ type: FILTER_BY_TOURIST_ACTIVITY, payload: json })
   }
 }
 
