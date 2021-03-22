331de9b86186f69452ba6be79725fe87eff9c4c2
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-22 05:30:27 -0500

    Modify component Home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from "react"
 import NavBar from "../NavBar/NavBar"
+// import style from "./Home.module.css"
 
 export default function Home() {
   return (
-    <div className="nav-bar">
+    <div>
       <NavBar />
     </div>
   )
