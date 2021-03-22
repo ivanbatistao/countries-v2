697b9ea83cdad70ffb537f402a95d22bf81b9a55
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-22 05:28:08 -0500

    Modify component Country

--- client/src/components/Country/Country.js
+++ client/src/components/Country/Country.js
@@ -1,18 +1,18 @@
 import React from "react"
-import { Fragment } from "react"
 import { Link } from "react-router-dom"
+import style from "./Country.module.css"
 
 export default function Card({ name, flag, continent, id }) {
   return (
-    <Fragment>
-      <img src={flag} alt="flag"></img>
-      <div className="#">
+    <div className={style.card}>
+      <img className={style.img} src={flag} alt="flag"></img>
+      <div className={style.content}>
         <h1>{name}</h1>
         <p>{continent}</p>
         <Link to={`/country/${id}`}>
           <button>Details</button>
         </Link>
       </div>
-    </Fragment>
+    </div>
   )
 }
