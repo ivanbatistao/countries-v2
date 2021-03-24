1c4c64afc3733eaa5aa01266e319ada33fa04b5e
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-24 07:46:53 -0500

    Add component OrderButtonPopulation

---
+++ client/src/components/OrderButtonPopulation/OrderButtonPopulation.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { orderPopulation } from "../../actions/index"
+
+import style from "./OrderButtonPopulation.module.css"
+
+function OrderButtonPopulation() {
+  const dispatch = useDispatch()
+  const { orderPop } = useSelector((state) => state)
+
+  console.log(orderPop)
+  function handleClick(e) {
+    e.preventDefault()
+    if (orderPop === true) {
+      dispatch(orderPopulation(false))
+    } else {
+      dispatch(orderPopulation(true))
+    }
+  }
+
+  return (
+    <Fragment>
+      <button
+        className={style.searchSubmit}
+        type="button"
+        onClick={(e) => handleClick(e)}
+      >
+        ORDER THIS PAGE BY POPULATION
+      </button>
+    </Fragment>
+  )
+}
+
+export default OrderButtonPopulation
