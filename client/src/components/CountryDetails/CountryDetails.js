672dd13557a3182291ab6f5c4916159bffe5cbd2
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-24 07:15:19 -0500

    Add new details to the component CountryDetails

--- client/src/components/CountryDetails/CountryDetails.js
+++ client/src/components/CountryDetails/CountryDetails.js
@@ -1,8 +1,9 @@
 import React, { Fragment, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useParams, Link } from "react-router-dom"
-// import style from './Cards.module.css'
 import { getCountryDetails } from "../../actions/index"
+
+import style from "./CountryDetails.module.css"
 
 function CountryDetails() {
   const { id } = useParams()
@@ -14,34 +15,37 @@ function CountryDetails() {
   }, [])
 
   return (
-    <Fragment>
-      <div className="country-detail">
-        <img src={`https://restcountries.eu/data/${id.toLowerCase()}.svg`}/>
-        <h1>{countryDetails.name}</h1>
-        <p>{countryDetails.id}</p>
-        <p>{countryDetails.capital}</p>
-        <p>{countryDetails.subregion}</p>
-        <p>{`${countryDetails.area} km2`}</p>
-        <p>{`${countryDetails.population} habitants`}</p>
-        <div>
+    <div className={style.containerCards}>
+      <div className={style.card}>
+        <img className={style.img} src={`https://restcountries.eu/data/${id.toLowerCase()}.svg`} />
+        <h1 className={style.name}>{countryDetails.name}</h1>
+        <p className={style.p}>Code: {countryDetails.id}</p>
+        <p className={style.p}>Capital: {countryDetails.capital}</p>
+        <p className={style.p}>Subregion: {countryDetails.subregion}</p>
+        <p className={style.p}>Area: {`${countryDetails.area} km2`}</p>
+        <p className={style.p}>Population: {`${countryDetails.population} habitants`}</p>
+        <div className={style.activities}>Tourist activities: 
           {countryDetails.touristActivities?.length > 0 ? (
             countryDetails.touristActivities.map((touristActivity, id) => (
               <div key={id}>
-                <p>{touristActivity.name}</p>
+                <p className={style.p}>{id+1}. {touristActivity.name}</p>
+                <p className={style.p2}>- Difficulty: {touristActivity.difficulty}</p>
+                <p className={style.p2}>- Duration: {touristActivity.duration}</p>
+                <p className={style.p2}>- Season: {touristActivity.season}</p>
               </div>
             ))
           ) : (
-            <p>This country doesn't have tourist activities</p>
+            <p className={style.p}>This country doesn't have tourist activities</p>
           )}
         </div>
         <Link to="/countries">
-          <button onClick="window.location.refresh=true">Back Home</button>
+          <button className={style.buttom} onClick="window.location.refresh=true">Back Home</button>
         </Link>
         <Link to="/countries/addactivity">
-          <div>Add Activity</div>
+          <input className={style.buttom} type="button" value="Add Activity" />
         </Link>
       </div>
-    </Fragment>
+    </div>
   )
 }
 
