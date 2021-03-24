313106be777dea0cf6587810d4a7655c3c7c12b8
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-24 07:22:02 -0500

    Modify NavBar component

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,14 +1,17 @@
-import React from "react";
-import Search from "../Search/Search";
+import React from "react"
 import { Link } from "react-router-dom"
+import style from "./NavBar.module.css"
 
 export default function NavBar() {
   return (
-    <div className="nav-bar" >
-        <Search />
-        <Link to={`/countries/addactivity`}>
-          <div>Add Tourist Activity</div>
-        </Link>
+    <div title="container" className={style.home}>
+      <div className={style.nameApp}>Where in the world?</div>
+      <Link className={style.link} to="/">
+        <div>Welcome</div>
+      </Link>
+      <Link className={style.link} to={`/countries/addactivity`}>
+        <div>Add tourist activity</div>
+      </Link>
     </div>
   )
 }
