6154e0a57c4e49c707f64dcfcbc5500f262173c3
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-24 07:08:45 -0500

    Add AddActivity component

--- client/src/components/AddActivity/AddActivity.js
+++ client/src/components/AddActivity/AddActivity.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
+
 import style from "./AddActivity.module.css"
 
 function AddActivity() {
@@ -31,7 +32,7 @@ function AddActivity() {
         season === "" ||
         countries === ""
       ) {
-        alert("Fill all the fields")
+        return alert("Fill all the fields")
       } else {
         const response = await fetch("http://localhost:3001/activity", {
           method: "POST",
@@ -52,67 +53,78 @@ function AddActivity() {
   }
 
   return (
-    <form className={style.form} onSubmit={(e) => handleSubmit(e)}>
+    <div className={style.form}>
       <div>
-        <label>Activity:</label>
-        <input
-          type="text"
-          name="name"
-          placeholder="Type an activity here"
-          onChange={handleInputChange}
-          value={input.name}
-        />
+        <h1>ADD A NEW TOURIST ACTIVITY</h1>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <label>Activity: </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Type an activity here"
+              onChange={handleInputChange}
+              value={input.name}
+            />
+          </div>
+          <div>
+            <label>Difficulty: </label>
+            <input
+              type="number"
+              step="0"
+              min="1"
+              max="5"
+              name="difficulty"
+              onChange={handleInputChange}
+              value={input.difficulty}
+            />
+          </div>
+          <div>
+            <label>Duration (hours): </label>
+            <input
+              type="number"
+              step="0.01"
+              min="0"
+              name="duration"
+              onChange={handleInputChange}
+              value={input.duration}
+            />
+          </div>
+          <div>
+            <label Forhtml="season">Season: </label>
+            <select name="season" id="season" onChange={handleInputChange}>
+              <option value="">Choose a season</option>
+              <option value="Summer">Summer</option>
+              <option value="Fall">Fall</option>
+              <option value="Winter">Winter</option>
+              <option value="Spring">Spring</option>
+            </select>
+          </div>
+          <div>
+            <label>Countries codes: </label>
+            <input
+              type="text"
+              name="countries"
+              placeholder="ex: COL, MEX"
+              onChange={handleInputChange}
+              value={input.countries}
+            />
+          </div>
+          <div className={style.bottoms}>
+            <div>
+              <input className={style.submit} type="submit" value="SUBMIT" />
+            </div>
+              <Link to="/countries">
+                <input
+                  className={style.backHome}
+                  type="submit"
+                  value="BACK HOME"
+                />
+              </Link>
+          </div>
+        </form>
       </div>
-      <div>
-        <label>Difficulty:</label>
-        <input
-          type="number"
-          step="0"
-          min="1"
-          max="5"
-          name="difficulty"
-          onChange={handleInputChange}
-          value={input.difficulty}
-        />
-      </div>
-      <div>
-        <label>Duration (hours):</label>
-        <input
-          type="number"
-          step="0.01"
-          min="0"
-          name="duration"
-          onChange={handleInputChange}
-          value={input.duration}
-        />
-      </div>
-      <div>
-        <label Forhtml="season">Season</label>
-        <select name="season" id="season" onChange={handleInputChange}>
-          <option value="">Choose a season</option>
-          <option value="Summer">Summer</option>
-          <option value="Fall">Fall</option>
-          <option value="Winter">Winter</option>
-          <option value="Spring">Spring</option>
-        </select>
-      </div>
-      <div>
-        <label>Countries codes:</label>
-        <input
-          type="text"
-          name="countries"
-          placeholder="Ex: COL, MEX"
-          onChange={handleInputChange}
-          value={input.countries}
-        />
-      </div>
-      <div>
-        <input type="submit" value="Submit" />
-      </div>
-      <Link to="/countries">
-        <div>Back home</div>
-      </Link>
-    </form>
+    </div>
   )
 }
 
