f4d724e19dd31f81b599a3d94116c1f2cfb5bf94
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-27 10:18:33 -0500

    Modify actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,7 +8,8 @@ import {
   ORDER_ASC,
   ORDER_DESC,
   ORDER_POP,
-  GET_HOME
+  GET_HOME,
+  PAGE,
 } from "../actions/action-types"
 
 export function getTenCountries() {
@@ -31,7 +32,7 @@ export function getCountriesSearch(name) {
   return async function (dispatch) {
     const response = await fetch(`http://localhost:3001/allCountries`)
     const json = await response.json()
-    dispatch({ type: GET_COUNTRIES_SEARCH, payload: {json, name }})
+    dispatch({ type: GET_COUNTRIES_SEARCH, payload: { json, name } })
   }
 }
 
@@ -43,19 +44,34 @@ export function getCountryDetails(id) {
   }
 }
 
-export function getCountryPagination(page, continent) {
-  return async function (dispatch) {
-    const response = await fetch(
-      `http://localhost:3001/countries?page=${page}&&continent=${continent}`
-    )
-    const json = await response.json()
-    dispatch({ type: GET_PAGES, payload: json })
+export function getCountryPagination(page, continent, order, population) {
+  console.log("actions:", population, page)
+  if (population) {
+    return async function (dispatch) {
+      const response = await fetch(
+        `http://localhost:3001/countries?page=${page}&&population=${population}`
+      )
+      const json = await response.json()
+      dispatch({ type: GET_PAGES, payload: json })
+    }
+  } else {
+    return async function (dispatch) {
+      const response = await fetch(
+        `http://localhost:3001/countries?page=${page}&&continent=${continent}&&order=${order}`
+      )
+      const json = await response.json()
+      dispatch({ type: GET_PAGES, payload: json })
+    }
   }
+  
+  
 }
 
 export function filterByContinent(continent) {
   return async function (dispatch) {
-    const response = await fetch(`http://localhost:3001/allCountries?continent=${continent}`)
+    const response = await fetch(
+      `http://localhost:3001/allCountries?continent=${continent}`
+    )
     const json = await response.json()
     dispatch({ type: FILTER_BY_CONTINENT, payload: { json, continent } })
   }
@@ -67,31 +83,59 @@ export function filterByTouristActivity(touristActivity) {
       `http://localhost:3001/countries?activity=${touristActivity}`
     )
     const json = await response.json()
+    console.log("action", json)
     dispatch({ type: FILTER_BY_TOURIST_ACTIVITY, payload: json })
   }
 }
 
-export function orderAsc() {
-  return {
-    type: ORDER_ASC,
+export function orderAsc(page, continent, order) {
+  return async function (dispatch) {
+    const response = await fetch(
+      `http://localhost:3001/countries?page=${page}&&continent=${continent}&&order=${order}`
+    )
+    const json = await response.json()
+    dispatch({ type: ORDER_ASC, payload: json })
   }
 }
 
-export function orderDesc() {
-  return {
-    type: ORDER_DESC,
+export function orderDesc(page, continent, order) {
+  return async function (dispatch) {
+    const response = await fetch(
+      `http://localhost:3001/countries?page=${page}&&continent=${continent}&&order=${order}`
+    )
+    const json = await response.json()
+    dispatch({ type: ORDER_DESC, payload: json })
   }
 }
 
-export function orderPopulation(payload) {
-  return {
-    type: ORDER_POP,
-    payload
+export function orderPopulationAsc(page, population) {
+  return async function (dispatch) {
+    const response = await fetch(
+      `http://localhost:3001/countries?page=${page}&&population=${population}`
+    )
+    const json = await response.json()
+    dispatch({ type: ORDER_POP, payload: { json, population }})
+  }
+}
+
+export function orderPopulationDesc(page, population) {
+  return async function (dispatch) {
+    const response = await fetch(
+      `http://localhost:3001/countries?page=${page}&&population=${population}`
+    )
+    const json = await response.json()
+    dispatch({ type: ORDER_POP, payload: { json, population }})
   }
 }
 
 export function getHome() {
   return {
     type: GET_HOME,
+  }
+}
+
+export function changePage(page) {
+  return {
+    type: PAGE, payload: page 
   }
 }
