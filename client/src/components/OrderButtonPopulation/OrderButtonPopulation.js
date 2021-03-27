1b2a67c960320dd03c194bc28c475f3c7302836a
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-27 10:32:46 -0500

    Add conditional to render component OrderButtonPopulation

--- client/src/components/OrderButtonPopulation/OrderButtonPopulation.js
+++ client/src/components/OrderButtonPopulation/OrderButtonPopulation.js
@@ -1,33 +1,38 @@
 import React, { Fragment } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { orderPopulation } from "../../actions/index"
+import { orderPopulationAsc, orderPopulationDesc } from "../../actions/index"
 
 import style from "./OrderButtonPopulation.module.css"
 
 function OrderButtonPopulation() {
   const dispatch = useDispatch()
-  const { orderPop } = useSelector((state) => state)
 
-  function handleClick(e) {
+  const { page, continentState } = useSelector((state) => state)
+
+  function handleClickAsc(e) {
     e.preventDefault()
-    if (orderPop === true) {
-      dispatch(orderPopulation(false))
-    } else {
-      dispatch(orderPopulation(true))
-    }
+    dispatch(orderPopulationAsc(page, "ASC"))
   }
 
-  return (
+  function handleClickDesc(e) {
+    e.preventDefault()
+    dispatch(orderPopulationDesc(page, "DESC"))
+  }
+
+  if (continentState !== "all" && continentState !== "") {
+    return <Fragment></Fragment>
+  }
+  
+  else {return (
     <Fragment>
-      <button
-        className={style.searchSubmit}
-        type="button"
-        onClick={(e) => handleClick(e)}
-      >
-        ORDER THIS PAGE BY POPULATION
-      </button>
-    </Fragment>
-  )
+    <button className={style.searchSubmit} type="button" onClick={(e) => handleClickAsc(e)}>
+      POP ASC
+    </button>
+    <button className={style.searchSubmit} type="button" onClick={(e) => handleClickDesc(e)}>
+      POP DESC
+    </button>
+  </Fragment>
+  ) }
 }
 
 export default OrderButtonPopulation
