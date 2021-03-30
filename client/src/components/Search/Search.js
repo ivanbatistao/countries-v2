8bd1dd34fb259cfcf727c84f4d74a88f9870f5d0
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-30 09:29:37 -0500

    Modify component Search.js - client

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,9 +1,9 @@
-import React, { Fragment, useState } from "react"
+import React, { useState } from "react"
+import { FaSearch } from "react-icons/fa"
 import { useDispatch } from "react-redux"
-import { getCountriesSearch } from "../../actions/index"
+import { getCountriesSearch, getTenCountries } from "../../actions/index"
 
-import style from './Search.module.css'
-
+import style from "./Search.module.css"
 
 export default function Search() {
   const dispatch = useDispatch()
@@ -13,29 +13,36 @@ export default function Search() {
   function handleChange(event) {
     setName(event.target.value)
 
-    // THIS LINE IS TO SEARCH DINAMICALLY
-      // dispatch(getCountriesSearch(name))
+    // THESE LINES ARE TO SEARCH DINAMICALLY
+    // if(event.target.value !== "") {
+    //   dispatch(getCountriesSearch(name))
+    // } else {
+    //   dispatch(getTenCountries())
+    // }
   }
 
   function handleSubmit(event) {
     event.preventDefault()
-      dispatch(getCountriesSearch(name))
-      setName("")
+    dispatch(getCountriesSearch(name))
+    setName("")
   }
 
   return (
-    <Fragment>
-      <form className={style.searchCountryButtom} onSubmit={(e) => handleSubmit(e)}>
-        <input
-          className={style.inputSearch}
-          placeholder=" Type a country name..."
-          type="text"
-          value={name}
-          onChange={(e) => handleChange(e)}
-          required
-        />
-        <input className={style.searchSubmit} type="submit" value="SEARCH FOR A COUNTRY" />
-      </form>
-    </Fragment>
+    <form
+      className={style.searchCountryButtom}
+      onSubmit={(e) => handleSubmit(e)}
+    >
+      <input
+        className={style.inputSearch}
+        placeholder=" Type a country name..."
+        type="text"
+        value={name}
+        onChange={(e) => handleChange(e)}
+        required
+      />
+      <button type="submit" className={style.searchSubmit}>
+        <FaSearch />
+      </button>
+    </form>
   )
 }
