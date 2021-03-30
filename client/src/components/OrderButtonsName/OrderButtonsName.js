8d6c5f92bdfd77af119fcfa5f342e94e78b977fb
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-30 09:28:49 -0500

    Modify component OrderButtonsName - client

--- client/src/components/OrderButtonsName/OrderButtonsName.js
+++ client/src/components/OrderButtonsName/OrderButtonsName.js
@@ -1,15 +1,15 @@
-import React, { Fragment } from "react"
+import React from "react"
+import { AiOutlineSortAscending, AiOutlineSortDescending } from "react-icons/ai"
 import { useDispatch, useSelector } from "react-redux"
 import { orderAsc, orderDesc } from "../../actions/index"
 
 import style from "./OrderButtonsName.module.css"
 
-
 function OrderButtons() {
   const dispatch = useDispatch()
 
   const { page, continentState } = useSelector((state) => state)
-  
+
   function handleClickAsc(e) {
     e.preventDefault()
     dispatch(orderAsc(page, continentState, "ASC"))
@@ -21,14 +21,24 @@ function OrderButtons() {
   }
 
   return (
-    <Fragment>
-      <button className={style.searchSubmit} type="button" onClick={(e) => handleClickAsc(e)}>
-        A-Z
+    <div className={style.containerName}>
+      <button
+        title="Order countries ascendent by name"
+        className={style.searchSubmit}
+        type="button"
+        onClick={(e) => handleClickAsc(e)}
+      >
+        <AiOutlineSortAscending />
       </button>
-      <button className={style.searchSubmit} type="button" onClick={(e) => handleClickDesc(e)}>
-        Z-A
+      <button
+        title="Order countries descendent by name"
+        className={style.searchSubmit}
+        type="button"
+        onClick={(e) => handleClickDesc(e)}
+      >
+        <AiOutlineSortDescending />
       </button>
-    </Fragment>
+    </div>
   )
 }
 
