d5e1dc5728b80bdf96684108c2017806fefacd6f
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-30 09:22:09 -0500

    Modify component CountryDetails - client

--- client/src/components/CountryDetails/CountryDetails.js
+++ client/src/components/CountryDetails/CountryDetails.js
@@ -17,32 +17,65 @@ function CountryDetails() {
   return (
     <div className={style.containerCards}>
       <div className={style.card}>
-        <img className={style.img} src={`https://restcountries.eu/data/${id.toLowerCase()}.svg`} alt="img-country" />
+        <img
+          className={style.img}
+          src={`https://restcountries.eu/data/${id.toLowerCase()}.svg`}
+          alt="img-country"
+        />
         <h1 className={style.name}>{countryDetails.name}</h1>
-        <p className={style.p}>Code: {countryDetails.id}</p>
-        <p className={style.p}>Capital: {countryDetails.capital}</p>
-        <p className={style.p}>Subregion: {countryDetails.subregion}</p>
-        <p className={style.p}>Area: {`${countryDetails.area} km2`}</p>
-        <p className={style.p}>Population: {`${countryDetails.population} habitants`}</p>
-        <div className={style.activities}>Tourist activities: 
-          {countryDetails.touristActivities?.length > 0 ? (
-            countryDetails.touristActivities.map((touristActivity, id) => (
-              <div key={id}>
-                <p className={style.p}>{id+1}. {touristActivity.name}</p>
-                <p className={style.p2}>- Difficulty: {touristActivity.difficulty}</p>
-                <p className={style.p2}>- Duration: {touristActivity.duration}</p>
-                <p className={style.p2}>- Season: {touristActivity.season}</p>
-              </div>
-            ))
-          ) : (
-            <p className={style.p}>This country doesn't have tourist activities</p>
-          )}
+        <p className={style.p}>
+          <span className={style.span}>CODE: </span> {countryDetails.id}
+        </p>
+        <p className={style.p}>
+          <span className={style.span}>CAPITAL: </span> {countryDetails.capital}
+        </p>
+        <p className={style.p}>
+          <span className={style.span}>SUBREGION: </span>{" "}
+          {countryDetails.subregion}
+        </p>
+        <p className={style.p}>
+          <span className={style.span}>AREA: </span>{" "}
+          {`${countryDetails.area} km2`}
+        </p>
+        <p className={style.p}>
+          <span className={style.span}>POPULATION: </span>{" "}
+          {`${countryDetails.population} habitants`}
+        </p>
+        <div className={style.activities}>
+          <b>Tourist Activities:</b>
+          <div className={style.touristActivitiesContainer}>
+            {countryDetails.touristActivities?.length > 0 ? (
+              countryDetails.touristActivities.map((touristActivity, id) => (
+                <div key={id}>
+                  <p className={style.p}>
+                    {id + 1}. {touristActivity.name}
+                  </p>
+                  <p className={style.p2}>
+                    - Difficulty: {touristActivity.difficulty}
+                  </p>
+                  <p className={style.p2}>
+                    - Duration: {touristActivity.duration}
+                  </p>
+                  <p className={style.p2}>- Season: {touristActivity.season}</p>
+                </div>
+              ))
+            ) : (
+              <p className={style.p}>
+                This country doesn't have tourist activities
+              </p>
+            )}
+          </div>
         </div>
         <Link to="/countries">
-          <button className={style.buttom} onClick="window.location.refresh=true">Back Home</button>
+          <button
+            className={style.buttom}
+            onClick="window.location.refresh=true"
+          >
+            BACK HOME
+          </button>
         </Link>
         <Link to="/countries/addactivity">
-          <input className={style.buttom} type="button" value="Add Activity" />
+          <input className={style.buttom} type="button" value="ADD ACTIVITY" />
         </Link>
       </div>
     </div>
