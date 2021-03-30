dbbe353864dcbc8131c420887b6647e1973e7317
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-30 09:31:43 -0500

    Add component Footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import style from "./Footer.module.css"
+
+export default function Footer() {
+  return (
+    <footer className={style.footer}>
+      <div>
+        &copy; Developed with <span>❤️</span> by{" "}
+        <a className={style.text} href="https://linkedin.com/in/ivanbatistao" target="_blank">
+          Ivan E. Batista-Ochoa.
+        </a>
+      </div>
+    </footer>
+  )
+}
