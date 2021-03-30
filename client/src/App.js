bab4e974dcc48bf6500bb66d5ee22293106d40c0
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-30 09:17:55 -0500

    Modify general styles in App - client

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import style from "./App.module.css"
 import { Route } from "react-router-dom"
 
 import InitialPage from "./components/InitialPage/InitialPage"
-import Home from "./components/Home/Home"
+import NavBar from "./components/NavBar/NavBar"
+import Footer from "./components/Footer/Footer"
 import Search from "./components/Search/Search"
 import Pages from "./components/Pages/Pages"
 import OrderButtonsName from "./components/OrderButtonsName/OrderButtonsName"
@@ -15,44 +16,84 @@ import Countries from "./components/Countries/Countries"
 import CountryDetails from "./components/CountryDetails/CountryDetails"
 
 import AddActivity from "./components/AddActivity/AddActivity"
-import NavBarWelcome from "./components/NavBarWelcome/NavBarWelcome"
 import OrderButtonPopulation from "./components/OrderButtonPopulation/OrderButtonPopulation"
 
 function App() {
   return (
     <div className={style.main}>
       <Route exact path="/">
-        <div className={style.initialPage}>
-          <NavBarWelcome />
-          <InitialPage />
+        <NavBar />
+        <InitialPage />
+        <div className={style.footer}>
+          <Footer />
         </div>
       </Route>
       <Route exact path="/countries">
-        <header className={style.AppHeader}>
-          <Home />
-        </header>
-        <div className={style.searchFilterActivity}>
-          <Search />
-          <FilterByTouristActivity />
+        <NavBar />
+        <div className={style.filtersOrders}>
+          <div className={style.searchFilterActivity}>
+            <Search />
+            <FilterByTouristActivity />
+          </div>
+          <div className={style.AscDesc}>
+            <OrderButtonsName />
+            <OrderButtonPopulation />
+          </div>
         </div>
-        <div>
+        <div className={style.containerPagesContinent}>
           <Pages />
-        </div>
-        <div className={style.chooseContinent}>
           <FilterByContinent />
         </div>
-        <div className={style.orderButtom1}>
-          <OrderButtonsName />
-        </div>
-        <div className={style.orderButtom2}>
-          <OrderButtonPopulation />
-        </div>
         <Countries />
+        <Footer />
       </Route>
-      <Route path="/countries/addactivity" component={AddActivity} />
-      <Route path="/country/:id" component={CountryDetails} />
+      <Route path="/countries/addactivity">
+        <AddActivity />
+        <Footer />
+      </Route>
+      <Route path="/country/:id">
+        <CountryDetails />
+        <Footer />
+      </Route>
     </div>
   )
 }
 
 export default App
+
+// function App() {
+//   return (
+//     <div className={style.main}>
+//       <Route exact path="/">
+//         <div className={style.initialPage}>
+//           <NavBarWelcome />
+//           <InitialPage />
+//         </div>
+//       </Route>
+//       <Route exact path="/countries">
+//         <header className={style.AppHeader}>
+//           <Home />
+//         </header>
+//         <div className={style.searchFilterActivity}>
+//           <Search />
+//           <FilterByTouristActivity />
+//         </div>
+//         <div>
+//           <Pages />
+//         </div>
+//         <div className={style.chooseContinent}>
+//           <FilterByContinent />
+//         </div>
+//         <div className={style.orderButtom1}>
+//           <OrderButtonsName />
+//         </div>
+//         <div className={style.orderButtom2}>
+//           <OrderButtonPopulation />
+//         </div>
+//         <Countries />
+//       </Route>
+//       <Route path="/countries/addactivity" component={AddActivity} />
+//       <Route path="/country/:id" component={CountryDetails} />
+//     </div>
+//   )
+// }
