2f7c5ea8fa3f993dd4fabf58d81cde6dfac14130
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-30 09:26:13 -0500

    Modify NavBar to render in all routes

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -4,14 +4,31 @@ import style from "./NavBar.module.css"
 
 export default function NavBar() {
   return (
-    <div title="container" className={style.home}>
-      <div className={style.nameApp}>Where in the world?</div>
-      <Link className={style.link} to="/">
-        <div>Welcome</div>
-      </Link>
-      <Link className={style.link} to={`/countries/addactivity`}>
-        <div>Add tourist activity</div>
-      </Link>
-    </div>
+    <header className={style.header}>
+      <nav className={style.nav}>
+        <ul className={style.menuItems}>
+          <li className={style.imgLi}>
+            <Link to="/">
+              <img
+                className={style.img}
+                src="https://www.flaticon.com/premium-icon/icons/svg/2072/2072130.svg"
+              />
+            </Link>
+          </li>
+          <div className={style.rightMenu}>
+            <li className={style.li}>
+              <Link className={style.link} onClick={() => {window.location.href="/countries"}} >
+                HOME
+              </Link>
+            </li>
+            <li className={style.li}>
+              <Link className={style.link} to={`/countries/addactivity`}>
+                ADD TOURIST ACTIVITY
+              </Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </header>
   )
 }
