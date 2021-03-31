9cb88fe54149e0ff4d1c306b9c2f7006947cf8a4
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2021-03-30 19:47:55 -0500

    Add code to render according to what is in current

--- client/src/components/Countries/Countries.js
+++ client/src/components/Countries/Countries.js
@@ -14,15 +14,21 @@ function Countries() {
   }, [dispatch])
 
   if (!(current instanceof Array)) {
-    return (<div className={style.countries}>Country Not Found</div>)
+    if (current.message) {
+      return <div className={style.countries}>Didn't find a country with that tourist activity? Try a new search.</div>
+    } else {
+      return (
+        <div className={style.countries}>
+          Didn't find a country? Try a new search.
+        </div>
+      )
+    }
   }
 
   if (current.length > 0) {
     return (
       <div className={style.containerCards}>
-        {current.map((
-          country
-        ) => (
+        {current.map((country) => (
           <Country
             key={country.id}
             id={country.id}
@@ -36,60 +42,10 @@ function Countries() {
   } else {
     return (
       <div className={style.countries}>
-        <p>There are no countries to show</p>
+        <p>Loading...</p>
       </div>
     )
   }
 }
 
 export default Countries
-
-// import React, { useEffect } from "react"
-// import { connect } from "react-redux"
-// import Card from "../Card/Card"
-// // import style from './Cards.module.css'
-// import { getTenCountries } from "../../actions/index"
-
-// function Cards({ countries, getTenCountries }) {
-//   useEffect(() => {
-//     getTenCountries()
-//   }, [])
-
-//   if (countries) {
-//     return (
-//       <div className="#">
-//         {countries.map((
-//           country // con ? se pregunta si existe algo ahí
-//         ) => (
-//           <Card
-//             key={country.id}
-//             id={country.id}
-//             flag={country.flag}
-//             name={country.name}
-//             continent={country.continent}
-//           />
-//         ))}
-//       </div>
-//     )
-//   } else {
-//     return (
-//       <div>
-//         <p>'There are no countries to display'</p>
-//       </div>
-//     )
-//   }
-// }
-
-// function mapStateToProps(state) {
-//   return {
-//     countries: state.countries,
-//   }
-// }
-
-// function mapDispatchToProps(dispatch) {
-//   return {
-//     getAllCountries: () => dispatch(getAllCountries()),
-//   }
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Cards)
